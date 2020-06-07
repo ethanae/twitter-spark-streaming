6fa32a54575fa43bd07d09641b468a27f9ba36fe
ethanae <eemandien@protonmail.com>
2020-06-07 22:40:55 +0200

    - add tweet schema

--- packages/tweet-processor/build.sbt
+++ packages/tweet-processor/build.sbt
@@ -5,4 +5,5 @@ scalaVersion := "2.11.8"
 libraryDependencies ++= Seq(
   "org.apache.spark" % "spark-streaming-kafka-0-10_2.11" % "2.4.5",
   "org.apache.spark" % "spark-streaming_2.11" % "2.2.0",
+  "org.apache.spark" % "spark-sql_2.11" % "2.2.0"
 )
--- packages/tweet-processor/src/main/scala/TweetStreamProcessor.scala
+++ packages/tweet-processor/src/main/scala/TweetStreamProcessor.scala
@@ -1,42 +1,61 @@
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.StreamingContext._
 import org.apache.spark.streaming.{ Seconds, StreamingContext }
-import org.apache.spark.{ SparkConf, SparkContext }
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferBrokers
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.sql.types.{ StructType, StringType, StructField, DateType }
 
 object TweetStreamProcessor {
   def main(args: Array[String]) {
-    
     val kafkaParams = Map[String, Object](
       "bootstrap.servers" -> "localhost:9092",
       "key.deserializer" -> classOf[StringDeserializer],
       "value.deserializer" -> classOf[StringDeserializer],
       "group.id" -> "tweet-stream",
       "auto.offset.reset" -> "latest",
-      "enable.auto.commit" -> (false: java.lang.Boolean)
+      "enable.auto.commit" -> (false: java.lang.Boolean),
+      "fetch.message.max.bytes" -> (2097152: java.lang.Integer)
     )
-    val topics = Array("tweets")
 
-    val sparkConfig = new SparkConf()
-      .setAppName("Twitter Stream")
-      .setMaster("local[2]")
-    val sparkContext = new SparkContext(sparkConfig)
-    sparkContext.setLogLevel("ERROR")
-    
-    val streamingContext = new StreamingContext(sparkContext, Seconds(1))
+    val topics = Array("tweets")
+    val spark = SparkSession
+      .builder()
+      .master("local[2]")
+      .appName("Twitter Stream Processor")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val ssc = new StreamingContext(spark.sparkContext, Seconds(2))
     val stream = KafkaUtils.createDirectStream[String, String](
-      streamingContext,
+      ssc,
       PreferBrokers,
       Subscribe[String, String](topics, kafkaParams)
     )
 
-    val mappedTweets = stream.map(record => (record.key, record.value))
-    mappedTweets.print()
+    val schema = StructType(
+      Array(
+        StructField("created_at", DateType, false),
+        StructField("text", StringType, false),
+        StructField("user", StructType(Array(StructField("location", StringType, true))), false)
+      )
+    )
 
-    streamingContext.start()
-    streamingContext.awaitTermination()
+    stream.foreachRDD(rddRaw => {
+      val rdd = rddRaw.map(_.value.toString)
+      val df = spark.read.schema(schema).json(rdd)
+      df.createOrReplaceTempView("tweet")
+      val text = df.select("text")
+      val sqlDF = spark.sql("select `text` from tweet")
+      sqlDF.printSchema()
+      sqlDF.show
+    })
+
+    ssc.start()
+    ssc.awaitTermination()
   }
 }
