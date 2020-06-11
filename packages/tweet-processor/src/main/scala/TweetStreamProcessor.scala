b95cbce448a95f8326483af8edf6af36191381f5
ethanae <eemandien@protonmail.com>
2020-06-11 22:57:50 +0200

    - add levenshtein edit distance calculation - add word dictionary text file

--- packages/tweet-processor/src/main/scala/TweetStreamProcessor.scala
+++ packages/tweet-processor/src/main/scala/TweetStreamProcessor.scala
@@ -11,6 +11,8 @@ import org.apache.spark.sql.types.{ StructType, StringType, StructField, DateTyp
 import org.bson.Document
 import com.mongodb.spark._
 import com.mongodb.spark.config._
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.functions._
 
 object TweetStreamProcessor {
   def main(args: Array[String]) {
@@ -51,6 +53,10 @@ object TweetStreamProcessor {
         StructField("user", StructType(Array(StructField("location", StringType, true))), false)
       )
     )
+    
+    import spark.implicits._ 
+
+    val dictionary = spark.read.text("resources/dictionary.txt")
 
     stream.foreachRDD(rddRaw => {
       val rdd = rddRaw.map(_.value.toString)
@@ -59,7 +65,14 @@ object TweetStreamProcessor {
 
       val df = spark.read.schema(schema).json(rdd)
       df.createOrReplaceTempView("tweets")
+
+      val tweetWords = df.select("text")
+        .flatMap{ case Row(s: String) => s.split(" ") }
+        .map(_.trim)
+
+      val probableCorrectSpellings = tweetWords.crossJoin(dictionary).withColumn("LD", levenshtein(tweetWords.col("value"), dictionary.col("value")))
       
+      probableCorrectSpellings.filter($"LD" < 4).show
     })
 
     ssc.start()
